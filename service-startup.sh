f4169c8093da9f8742145aad88b4df54ff69778c
Anurag Roy <anuragr9847@gmail.com>
2020-10-21 10:06:02 +0530

    Add more files

--- service-startup.sh
+++ service-startup.sh
@@ -4,6 +4,12 @@
 ##
 ## starts some systemd services after install
 
+# Copy powertop.service from user_files
+sudo cp ./user_files/etc/systemd/system/powertop.service /etc/systemd/system/
+
+# Enable powertop service for auto tune
 systemctl enable powertop.service
+
+# Enable tor service
 systemctl enable tor.service
 
